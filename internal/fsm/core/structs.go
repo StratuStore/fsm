2328c229ea643de883cd519118a6c8aa7a44ae63
Ihor Butsulin <igor.b769@gmail.com>
2025-05-24 23:31:11 +0300

    feat: added prefinal data structure

--- internal/fsm/core/structs.go
+++ internal/fsm/core/structs.go
@@ -1,27 +1,40 @@
 package core
 
 import (
+	"github.com/mbretter/go-mongodb/types"
 	"time"
 )
 
+type PathElement struct {
+	ID   types.ObjectId `bson:"_id" json:"id"`
+	Name string         `bson:"name"`
+}
+
 type Directory struct {
-	ID          string      `json:"id" bson:"_id"`
-	UserID      string      `json:"userID" bson:"userID"`
-	Path        []string    `json:"path" bson:"path"`
-	CreatedAt   time.Time   `json:"createdAt" bson:"createdAt"`
-	UpdatedAt   time.Time   `json:"updatedAt" bson:"updatedAt"`
-	Public      bool        `json:"public" bson:"public"`
-	Directories []Directory `json:"directories" bson:"directories"`
-	Files       []File      `json:"files" bson:"files"`
+	ID                types.ObjectId `json:"id" bson:"_id,omitempty"`
+	UserID            string         `json:"userID" bson:"userID"`
+	ParentDirectoryID string         `bson:"parentDirectoryID" json:"parentDirectoryID,omitempty"`
+	Path              []PathElement  `json:"path" bson:"path"`
+	Name              string         `json:"name" bson:"name"`
+	CreatedAt         time.Time      `json:"createdAt" bson:"createdAt,omitempty"`
+	UpdatedAt         time.Time      `json:"updatedAt" bson:"updatedAt,omitempty"`
+	Public            bool           `json:"public" bson:"public"`
+	DirectoriesCount  uint           `json:"directoriesCount" bson:"directoriesCount"`
+	Directories       []Directory    `json:"directories" bson:"directories"`
+	FilesCount        uint           `json:"filesCount" bson:"filesCount"`
+	Files             []File         `json:"files" bson:"files"`
+	Size              uint           `json:"size" bson:"size"`
 }
 
 type File struct {
-	ID        string            `json:"id" bson:"_id"`
+	ID        types.ObjectId    `json:"id" bson:"_id,omitempty"`
 	UserID    string            `json:"userID" bson:"userID"`
 	CreatedAt time.Time         `json:"createdAt" bson:"createdAt"`
 	UpdatedAt time.Time         `json:"updatedAt" bson:"updatedAt"`
+	Starred   bool              `json:"starred" bson:"starred"`
+	Size      uint              `bson:"size" json:"size"`
 	Public    bool              `json:"public" bson:"public"`
 	Name      string            `json:"name" bson:"name"`
+	Extension string            `json:"extension" bson:"extension"`
 	Attrs     map[string]string `json:"attrs" bson:"attrs"`
-	Server    string            `json:"server" bson:"server"`
 }
