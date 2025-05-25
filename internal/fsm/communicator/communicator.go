bb0d3f307d2530455811cbb7360284b253839610
Ihor Butsulin <igor.b769@gmail.com>
2025-05-25 03:50:55 +0300

    feat: added communicator mock

---
+++ internal/fsm/communicator/communicator.go
@@ -0,0 +1,22 @@
+package communicator
+
+import "context"
+
+type Communicator struct {
+}
+
+func (c *Communicator) Delete(ctx context.Context, id string) error {
+	return nil
+}
+
+func (c *Communicator) Create(ctx context.Context, id string) (host, connectionID string, err error) {
+	return "localhost:5675", "3f122594-a43a-474b-85d2-36a8ce98ef2e", nil
+}
+
+func (c *Communicator) Open(ctx context.Context, id string) (host, connectionID string, err error) {
+	return "localhost:5675", "3f122594-a43a-474b-85d2-36a8ce98ef2e", nil
+}
+
+func (c *Communicator) Update(ctx context.Context, id string) (host, connectionID string, err error) {
+	return "localhost:5675", "3f122594-a43a-474b-85d2-36a8ce98ef2e", nil
+}
